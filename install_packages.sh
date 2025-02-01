4913ce1179acbbca50fed87752e4dcf6beb7b526
Gloria <gpmeneses@gmail.com>
2025-02-01 00:32:16 +0000

    :hammer: Add dependencies installation script.

---
+++ install_packages.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+python -m pip install --upgrade
+pip3 install -q anytree
+pip3 install -q --upgrade google-cloud-pubsub
+pip3 install -q faker
+pip3 install -q --upgrade geocoder
+pip3 install --upgrade google-cloud-storage
+pip3 install --upgrade 'apache-beam[gcp]'
